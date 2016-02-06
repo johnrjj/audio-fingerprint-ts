6f57b4b6904fd8fc1b5908109fbd68af1cd5b30c
John Johnson <johnrjj@gmail.com>
2016-02-05 22:19:26 -0500

    Update compile.js

--- test/compile.js
+++ test/compile.js
@@ -1,13 +1,3 @@
-// require('ts-node/register');
-// require("babel-register")({
-//   // module: {
-//     loaders: [{
-//       test: /\.tsx?$/,
-//       loaders: ['babel', 'ts'],
-//     }],
-//   // }
-//   extensions: [".es6", ".es", ".jsx", ".js", ".ts", ".tsx"]
-// });
 require('ts-node').register({
     project: 'test',
     exitOnError: true
